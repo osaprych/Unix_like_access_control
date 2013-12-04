8ee4692cb30b9e71d7a20f75de133314e1a1b93e
Olga <osaprych@gmail.com>
2013-12-04 14:57:31 -0600

    cleaned up comments

--- ACS.java
+++ ACS.java
@@ -2,11 +2,6 @@
 // UT EID os3587
 // CS ID osaprych
 
-/*
-Please note that since I'm using HashMap to store files, 
-the order in whitch the files are written to state.log file might be different from the order they were in fileList.
-*/
-
 import java.util.*;
 import java.io.*;
 
